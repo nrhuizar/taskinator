69652e1790b89f8f3c30f7b517f2f7c1cad04b2c
nrhuizar <nrhuizar@gmail.com>
2020-10-31 11:04:41 -0700

    cleaned up usability concerns

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
     // packae up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -14,6 +19,7 @@ var taskFormHandler = function(event) {
 
     // send it as an argument to createTaskE1
     createTaskE1(taskDataObj);
+    formE1.reset();
 }
 
 var createTaskE1 = function(taskDataObj) {
